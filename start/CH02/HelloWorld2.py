c43816c22680502b015d963bf215dafad9796f90
Nikita02071993 <Nikitaagr02@gmail.com>
2023-09-29 14:02:35 +0530

    2commit

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -5,3 +5,11 @@
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
+your_name = input("what is your name?")
+print(your_name)
+print("Hello" + your_name)
+print("Hello", your_name)
+message = "Hello" + your_name
+print(message)
+print("Hello{0}".format(your_name))
+print(f"Hello {your_name}")
