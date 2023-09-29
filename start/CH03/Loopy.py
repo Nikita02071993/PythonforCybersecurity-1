e403f73af3af70391159e9a471c0d872aa571c80
Nikita02071993 <Nikitaagr02@gmail.com>
2023-09-29 16:23:18 +0530

    assignment4

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Nikita
+What_you_think = input("Is today a good day? (y/n)")
+
+if What_you_think == "y":
+    for i in range(10):
+        print ("Yes it is")
