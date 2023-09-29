77ee315ad9a5ff60e9c178a04f245a982fa14256
Nikita02071993 <Nikitaagr02@gmail.com>
2023-09-29 15:41:10 +0530

    assignment2

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
 # Created 
+Name_please = input("What is your name")
+print("Hello" + Name_please + " Today is going to be a great day!")
+Age_please = int(input("What is your age"))
+print("In 2 years, you will be", (Age_please + 2), "years old")
--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 # example workign with conditionals
-#By 
+#By Nikita
+
+What_you_think = input("Is today a good day? (y/n)")
+
+if What_you_think == "y":
+    print ("Yes it is")
+else:
+    print("Ahh! Its not")
